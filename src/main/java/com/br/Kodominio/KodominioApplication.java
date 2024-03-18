dba61fb27ba357011e9b7b65cbd9fb54fc4c937e
CeloJay <jmgamer.360@gmail.com>
2024-03-18 15:42:13 -0300

    Projeto vazio

---
+++ src/main/java/com/br/Kodominio/KodominioApplication.java
@@ -0,0 +1,13 @@
+package com.br.Kodominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodominioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/Kodominio/KodominioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.Kodominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
