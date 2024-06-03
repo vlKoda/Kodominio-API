e8e3181fc94001b5f07e6dabea530e538acbccc9
CeloJay <jmgamer.360@gmail.com>
2024-06-02 22:42:25 -0300

    Documentos Docker

--- cond-ci.cmd
+++ cond-ci.cmd
@@ -1,6 +1,6 @@
 docker stack rm kodominio
-docker build -t rbasstos/kodominio-api .
-docker push rbasstos/kodominio-api
-docker pull rbasstos/kodominio-api
+docker build -t kodominio-api .
+docker push kodominio-api
+docker pull kodominio-api
 docker stack deploy --compose-file stack.yml kodominio
 docker stack ps kodominio
