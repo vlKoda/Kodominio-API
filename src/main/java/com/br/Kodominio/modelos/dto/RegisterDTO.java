17804be48feb86d3f7a67276eb5bab1d035e42d3
CeloJay <jmgamer.360@gmail.com>
2024-06-05 21:41:12 -0300

    resolvi sabomba

--- src/main/java/com/br/Kodominio/modelos/dto/RegisterDTO.java
+++ src/main/java/com/br/Kodominio/modelos/dto/RegisterDTO.java
@@ -3,5 +3,5 @@ package com.br.Kodominio.modelos.dto;
 import com.br.Kodominio.modelos.entidades.Condominio;
 import com.br.Kodominio.modelos.role.Role;
 
-public record RegisterDTO(Long id, String nome, String email, String senha, String telefone, String condominio, String apartamento, Role role) {
+public record RegisterDTO(Long id, String nome, String email, String senha, String telefone, Condominio condominio, String apartamento, Role role) {
 }
