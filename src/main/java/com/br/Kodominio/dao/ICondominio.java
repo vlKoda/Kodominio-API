0862aa931219b3cc3008f8f68623e7ac594ce7db
CeloJay <jmgamer.360@gmail.com>
2024-06-12 15:22:32 -0300

    Problema do fetch resolvido

--- src/main/java/com/br/Kodominio/dao/ICondominio.java
+++ src/main/java/com/br/Kodominio/dao/ICondominio.java
@@ -1,12 +1,17 @@
 package com.br.Kodominio.dao;
 
 import com.br.Kodominio.modelos.entidades.Condominio;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @EnableJpaRepositories
 public interface ICondominio extends CrudRepository<Condominio, Integer> {
 
+    @Query("select distinct c from Condominio c join fetch c.usuarios")
+    List<Condominio> findAll();
 }
