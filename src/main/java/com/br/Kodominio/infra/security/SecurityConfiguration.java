796da3ca98a9e3fd5034054df78bda90b7239561
CeloJay <jmgamer.360@gmail.com>
2024-06-15 19:29:12 -0300

    security configuration

--- src/main/java/com/br/Kodominio/infra/security/SecurityConfiguration.java
+++ src/main/java/com/br/Kodominio/infra/security/SecurityConfiguration.java
@@ -1,10 +1,8 @@
 package com.br.Kodominio.infra.security;
 
-import com.br.Kodominio.controllers.AuthenticationController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
